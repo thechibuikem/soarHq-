2c0cefe2e6b30a3d4d08c170cfada73ffc776847
Chukwuemeka David <dev.ataraxia7@gmail.com>
2025-08-03 19:44:30 +0100

    refactor: standardize import formatting and component return structure in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
-import Hero from '../components/Hero'
-import TestimonialSection from '../components/testimonial';
-import MeetOurTeamSection from '../components/MeetOurTeam';
-import MissionSection from '../components/Mission';
-import Footer from '../components/Footer';
-import VisionStatement from '../components/VisionStatement';
+import Hero from "../components/Hero";
+import TestimonialSection from "../components/testimonial";
+import MeetOurTeamSection from "../components/MeetOurTeam";
+import MissionSection from "../components/Mission";
+import Footer from "../components/Footer";
+import VisionStatement from "../components/VisionStatement.jsx";
 
 const Home = () => {
-    return(
-        <>
-        <Hero />
-        <VisionStatement />
-        <TestimonialSection />
-        <MeetOurTeamSection />
-        <MissionSection />
-        <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Hero />
+      <VisionStatement />
+      <TestimonialSection />
+      <MeetOurTeamSection />
+      <MissionSection />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
