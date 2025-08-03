83e1a7882e052de66dfab9fccc27a1cddc3f1c67
Chukwuemeka David <dev.ataraxia7@gmail.com>
2025-08-03 10:40:35 +0100

    remove false book path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Elibrary from './pages/Elibrary'
 import Events from './pages/Events'
-import BookPage from './pages/book'
 import FlipbookViewer from './components/BookTemplate'
 const App = () => {
  
@@ -11,7 +10,6 @@ const App = () => {
     <Route path="/" element={<Home />} />
     <Route path="/Events" element={<Events />} />
     <Route path="/Elibrary" element={<Elibrary />} />
-    <Route path="/Book/" element={<BookPage />} />
     <Route path="/FlipBook" element={<FlipbookViewer/>}/>
   </Routes>
   )
