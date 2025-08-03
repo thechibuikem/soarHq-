a5e3700e0ff699d4288db4cb99ef99ab81001527
Chukwuemeka David <dev.ataraxia7@gmail.com>
2025-08-03 19:41:43 +0100

    fix: reorder import statement for VisionStatement in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import Hero from '../components/Hero'
-import VisionStatement from '../components/VisionStatement';
 import TestimonialSection from '../components/testimonial';
 import MeetOurTeamSection from '../components/MeetOurTeam';
 import MissionSection from '../components/Mission';
 import Footer from '../components/Footer';
+import VisionStatement from '../components/VisionStatement';
 
 const Home = () => {
     return(
