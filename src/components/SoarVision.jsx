f55642e5e87ca136ba0b0a8607491961cfc317d7
Chukwuemeka David <dev.ataraxia7@gmail.com>
2025-08-20 00:50:00 +0100

    touched a lil space

--- src/components/SoarVision.jsx
+++ src/components/SoarVision.jsx
@@ -1,7 +1,7 @@
 const SoarVision = () =>{
  return(
         //   <about us section starts
-      <section id="moreAboutSection" className=" flex flex-col gap-y-8 mx-auto justify-center items-center w-[80%] h-fit text-center mt-8 md:mt-12 lg:mt-16">
+      <section id="moreAboutSection" className=" flex flex-col gap-y-4 mx-auto justify-center items-center w-[80%] h-fit text-center mt-16 lg:mt-20">
         <div className="sm:text-[1.3rem] text-[1.2rem] tracking-[1px] font-normal text-[#2c2b2b] uppercase" >Vision Statement</div>
         <div className="sm:text-[1.65rem] text-[1.2rem] font-extralight color-[#2c2b2b]">
           We see a world where women identify as nation nurturers — soaring in
